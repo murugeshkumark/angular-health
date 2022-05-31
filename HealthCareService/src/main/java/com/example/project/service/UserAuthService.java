e5af615eb8661be58a5c2b9427283f14c616c87d
Admin <admin.push@hackerrank.workspace>
2022-05-31 21:32:13 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- HealthCareService/src/main/java/com/example/project/service/UserAuthService.java
+++ HealthCareService/src/main/java/com/example/project/service/UserAuthService.java
@@ -1,6 +1,7 @@
 package com.example.project.service;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +15,18 @@ import org.springframework.stereotype.Service;
 import com.example.project.Model.ApplicationUser;
 import com.example.project.repository.ApplicationUserRepository;
 
-
-
 @Service
-public class UserAuthService {
-	
+public class UserAuthService implements UserDetailsService {
+
+  @Autowired
+  ApplicationUserRepository applicationUserRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String arg0) throws UsernameNotFoundException {
+    ApplicationUser applicationUser = applicationUserRepository.findById(arg0).orElse(null);
+    if (applicationUser == null) {
+      throw new UsernameNotFoundException("user name not found " + arg0);
+    }
+    return null;
+  }
 }
